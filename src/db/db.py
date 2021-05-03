10ae354ed478f5b308023566f568b2720831e361
Fennec2000GH <qcaijun2013@gmail.com>
2021-05-02 22:58:01 -0400

    moved db.py to more specialized folder

---
+++ src/db/db.py
@@ -0,0 +1,48 @@
+
+import os
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from dotenv import load_dotenv
+
+# .env variables
+load_dotenv()
+DB_PATH = os.getenv('DB_PATH')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+print(DB_PATH)
+print(CLIENT_ID)
+print(CLIENT_SECRET)
+
+# other variables
+keyspace = 'PowerSearchKS'
+
+cloud_config= {
+        'secure_connect_bundle': DB_PATH
+}
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect(keyspace=keyspace)
+
+# row = session.execute("select release_version from system.local").one()
+# if row:
+#     print(row[0])
+# else:
+#     print("An error occurred.")
+
+table_query = \
+"""
+CREATE TABLE IF NOT EXISTS history (
+   id int PRIMARY KEY AUTO,
+   user_id int,
+   url text,
+   summary text,
+   sentiment_score double,
+   sentiment_magnitude double,
+   entities set<text>
+)
+"""
+
+
